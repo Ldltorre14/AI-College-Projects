42f1051e93dae298f17a29e1622d2d49c56db5f6
Leonardo Perez <83437994+Ldltorre14@users.noreply.github.com>
2023-03-11 21:19:20 -0600

    Update graph.py

--- Vacuum Cleaner Simulator/graph.py
+++ Vacuum Cleaner Simulator/graph.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import matplotlib.pylab as plt
+from time import *
 
 class Node:
     def __init__(self,posX,posY,id):
@@ -22,18 +23,55 @@ class Graph:
         self.node8 = Node(550,475,'I')
         self.node9 = Node(350,550,'J')
         self.node10 = Node(350,670,'K')
-        self.edges = {self.startNode.id:{self.node1.id:100,self.node2.id:100,self.node3.id:150},
-                      self.node1.id:{self.startNode.id:100,self.node2.id:200,self.node4.id:200,self.node6.id:250},
-                      self.node2.id:{self.startNode.id:100,self.node1.id:200,self.node5.id:200,self.node6.id:250},
-                      self.node3.id:{self.startNode.id:100,self.node6.id:100},
-                      self.node4.id:{self.node1.id:100,self.node5.id:200,self.node6.id:100,self.node7.id:100},
-                      self.node5.id:{self.node2.id:100,self.node4.id:200,self.node6.id:100,self.node8.id:100},
-                      self.node6.id:{self.node1.id:250,self.node2.id:250,self.node3.id:100,self.node4.id:100,self.node5.id:100,self.node7.id:100,self.node8.id:100,self.node9.id:100},
-                      self.node7.id:{self.node4.id:100,self.node6.id:100,self.node8.id:200,self.node9.id:100},
-                      self.node8.id:{self.node5.id:100,self.node6.id:100,self.node7.id:200,self.node9.id:100},
-                      self.node9.id:{self.node7.id:100,self.node8.id:100,self.node6.id:100},
-                      self.node10.id:{self.node9.id:100}
+        self.edges = {self.startNode:{self.node1:100,self.node2:100,self.node3:150},
+                      self.node1:{self.startNode:100,self.node2:200,self.node4:200,self.node6:250},
+                      self.node2:{self.startNode:100,self.node1:200,self.node5:200,self.node6:250},
+                      self.node3:{self.startNode:100,self.node6:100},
+                      self.node4:{self.node1:100,self.node5:200,self.node6:100,self.node7:100},
+                      self.node5:{self.node2:100,self.node4:200,self.node6:100,self.node8:100},
+                      self.node6:{self.node1:250,self.node2:250,self.node3:100,self.node4:100,self.node5:100,self.node7:100,self.node8:100,self.node9:100},
+                      self.node7:{self.node4:100,self.node6:100,self.node8:200,self.node9:100,self.node10:100},
+                      self.node8:{self.node5:100,self.node6:100,self.node7:200,self.node9:100,self.node10:100},
+                      self.node9:{self.node7:100,self.node8:100,self.node6:100,self.node10:100},
+                      self.node10:{self.node9:100}
                       }
+
+house = Graph()
+toVisit = [house.startNode]
+visited = 0
+              
+                
+def traverse(house):
+    toVisit = [house.startNode]
+    visited = []
+    distance = 0
+    i = 0
+    #We traverse through the toVisit nodes (inittialy only startNode)
+    while toVisit:
+        n = toVisit.pop(0)
+        print("Visiting node...",n.id)
+        if not n.status:
+            #Label for indicating the cleaning process
+            print("cleaning...")
+            sleep(0.5)
+            n.status = True
+            #Label for indication the node was cleaned
+            print("Clean!")
+            #Change the node color to green (Clean)
+            sleep(0.5)
+            visited.append(n)
+            for o in house.edges[n]:
+                toVisit.append(o)
+                distance += house.edges[n][o]
+    
+    for x in visited:
+        print(x.id)
+    print(distance)
+    
+#traverse(house)          
+                 
+    
+    
 
 
 #House = Graph()
@@ -49,8 +87,3 @@ class Graph:
 
 
         
-
-
-
-        
-
