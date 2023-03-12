609fd4dbd1700aa33043ca0b4b133f37188d8760
Leonardo Perez <83437994+Ldltorre14@users.noreply.github.com>
2023-03-11 21:16:29 -0600

    Update AI.py

--- Vacuum Cleaner Simulator/AI.py
+++ Vacuum Cleaner Simulator/AI.py
@@ -1,8 +1,12 @@
 from graph import Node
+from graph import Graph
+
 
 class vacuumCleaner(Node):
     def __init__(self):
         self.score = 0
         self.traveled_Distance = 0
-        self.previousConfig = [] 
-    
+        self.previousConfig = []
+        self.toVisit = []
+        self.visited = []
+        
